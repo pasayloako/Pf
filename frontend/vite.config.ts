d91a281f05d89a8c635eefd44f264a95d03c38d9
pasayloako <pasayloako6@gmail.com>
2026-05-01 13:24:25 +0800

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,17 +1,8 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3000',
-        changeOrigin: true,
-      }
-    }
-  },
   build: {
     outDir: 'dist',
-    sourcemap: false,
-    minify: 'esbuild'  // Just change this ONE line from 'terser' to 'esbuild'
+    minify: false
   }
 });
