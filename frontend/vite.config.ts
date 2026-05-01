a30e05d754fe5ba5ade1656be8c529ab3a1efc8f
pasayloako <pasayloako6@gmail.com>
2026-05-01 12:30:58 +0800

    Create vite.config.ts

---
+++ frontend/vite.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+      }
+    }
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        drop_console: true,
+      },
+    },
+  },
+});
